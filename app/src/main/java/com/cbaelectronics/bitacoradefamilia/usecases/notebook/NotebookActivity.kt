8ba3a5cca78b6d2a90a7d7730ff34cac61a884cb
Cba Electronics <eduardosanchez77@gmail.com>
2023-03-29 16:04:39 -0300

    Complete

--- app/src/main/java/com/cbaelectronics/bitacoradefamilia/usecases/notebook/NotebookActivity.kt
+++ app/src/main/java/com/cbaelectronics/bitacoradefamilia/usecases/notebook/NotebookActivity.kt
@@ -5,9 +5,14 @@
 
 package com.cbaelectronics.bitacoradefamilia.usecases.notebook
 
+import android.app.Dialog
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
@@ -15,10 +20,17 @@ import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
+import com.airbnb.lottie.LottieAnimationView
 import com.cbaelectronics.bitacoradefamilia.R
 import com.cbaelectronics.bitacoradefamilia.databinding.ActivityNotebookBinding
 import com.cbaelectronics.bitacoradefamilia.model.domain.Children
+import com.cbaelectronics.bitacoradefamilia.model.domain.Permission
 import com.cbaelectronics.bitacoradefamilia.provider.services.firebase.DatabaseField
+import com.cbaelectronics.bitacoradefamilia.usecases.about.AboutRouter
+import com.cbaelectronics.bitacoradefamilia.usecases.onboarding.OnboardingRouter
+import com.cbaelectronics.bitacoradefamilia.usecases.settings.SettingsRouter
+import com.cbaelectronics.bitacoradefamilia.usecases.share.ShareRouter
+import com.cbaelectronics.bitacoradefamilia.util.UIUtil
 import com.cbaelectronics.bitacoradefamilia.util.extension.addClose
 
 class NotebookActivity : AppCompatActivity() {
@@ -30,15 +42,76 @@ class NotebookActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityNotebookBinding.inflate(layoutInflater)
+
+        // Content
         setContentView(binding.root)
+
+        // ViewModel
+        viewModel = ViewModelProvider(this)[NotebookViewModel::class.java]
+
+        // Setup
+        data()
+        setup()
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.action_share -> {
+                if(viewModel.children?.permission == Permission.ADMIN.value){
+                    ShareRouter().launch(this)
+                }else{
+                    UIUtil.showSnackBar(binding.container, getString(viewModel.alertShared))
+                }
+            }
+            R.id.action_onboard -> {
+                OnboardingRouter().launch(this)
+            }
+            R.id.action_about -> {
+                AboutRouter().launch(this)
+            }
+            R.id.action_settings -> {
+                SettingsRouter().launch(this)
+            }
+            R.id.action_close -> {
+                loadAlertDialog()
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        overridePendingTransition(R.anim.slide_back_in_up, R.anim.slide_back_out_up)
+    }
+
+    // Private
+
+    private fun data(){
+        val bundle = intent.extras
+        childrenJSON = bundle?.getString(DatabaseField.CHILDREN.key).toString()
+        children = Children.fromJson(childrenJSON)!!
+    }
+
+
+    private fun setup() {
+        addClose(this)
 
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_notebook)
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
                 R.id.navigation_achievements,
@@ -50,49 +123,40 @@ class NotebookActivity : AppCompatActivity() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
-
-        // ViewModel
-        viewModel = ViewModelProvider(this)[NotebookViewModel::class.java]
-
-        // Setup
-        data()
-        setup()
     }
 
-    private fun data(){
-        val bundle = intent.extras
-        childrenJSON = bundle?.getString(DatabaseField.CHILDREN.key).toString()
-        children = Children.fromJson(childrenJSON)!!
-    }
+    private fun loadAlertDialog() {
+        val mDialog = Dialog(binding.root.context)
+        val mWindows = mDialog.window!!
+
+        mWindows.attributes.windowAnimations = R.style.DialogAnimation
+        mDialog.setContentView(R.layout.custom_dialog_opciones)
+        mDialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        mDialog.setCancelable(false)
+        val mText = mDialog.findViewById<TextView>(R.id.txtDialog)
+        val mBtnOK = mDialog.findViewById<Button>(R.id.btnDialogAcept)
+        val mBtnCancel = mDialog.findViewById<Button>(R.id.btnDialogCancel)
+        val lottieDialog = mDialog.findViewById<LottieAnimationView>(R.id.lottieDialog)
+
+        mText.text = getString(viewModel.alertLogout)
+        lottieDialog.setAnimation(R.raw.logout)
 
 
-    private fun setup() {
-        addClose(this)
-    }
+        lottieDialog.loop(true)
+        lottieDialog.playAnimation()
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.menu_main, menu)
-        return true
-    }
+        mBtnOK.text = getString(viewModel.alertButtonOk)
+        mBtnCancel.text = getString(viewModel.alertButtonCancel)
 
-    override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        return when (item.itemId) {
-            R.id.action_settings -> true
-            else -> super.onOptionsItemSelected(item)
+        mDialog.show()
+
+        mBtnOK.setOnClickListener {
+            mDialog.cancel()
+            viewModel.close(this)
         }
-    }
+        mBtnCancel.setOnClickListener {
+            mDialog.cancel()
 
-    override fun onSupportNavigateUp(): Boolean {
-        onBackPressed()
-        return super.onSupportNavigateUp()
-    }
-
-    override fun onBackPressed() {
-        super.onBackPressed()
-        overridePendingTransition(R.anim.slide_back_in_up, R.anim.slide_back_out_up)
+        }
     }
 }
--- app/src/main/java/com/cbaelectronics/bitacoradefamilia/usecases/notebook/NotebookViewModel.kt
+++ app/src/main/java/com/cbaelectronics/bitacoradefamilia/usecases/notebook/NotebookViewModel.kt
@@ -5,11 +5,15 @@
 
 package com.cbaelectronics.bitacoradefamilia.usecases.notebook
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
+import com.cbaelectronics.bitacoradefamilia.R
 import com.cbaelectronics.bitacoradefamilia.model.domain.Children
 import com.cbaelectronics.bitacoradefamilia.model.domain.User
 import com.cbaelectronics.bitacoradefamilia.model.domain.UserSettings
 import com.cbaelectronics.bitacoradefamilia.model.session.Session
+import com.cbaelectronics.bitacoradefamilia.provider.preferences.PreferencesProvider
+import com.cbaelectronics.bitacoradefamilia.usecases.login.LoginRouter
 
 class NotebookViewModel: ViewModel() {
 
@@ -19,5 +23,20 @@ class NotebookViewModel: ViewModel() {
     var settings = Session.instance.user?.settings ?: UserSettings()
     var children = Session.instance.children
     var permission = Session.instance.permission
+    var childrenShared: Children? = null
+
+    // Localization
+
+    val alertShared = R.string.menu_alert_notShared
+    val alertLogout = R.string.session_alert_logout
+    val alertButtonOk = R.string.session_alert_button_ok
+    val alertButtonCancel = R.string.session_alert_button_cancel
+
+    // Public
+
+    fun close(context: Context) {
+        PreferencesProvider.clear(context)
+        LoginRouter().launch(context)
+    }
 
 }
